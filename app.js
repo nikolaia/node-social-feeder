4f84a97cd0728b6d75920f0acf22fe4e4321aa54
nikolai-neovada <nikolai@stasj.com>
2013-07-23 15:59:47 +0200

    Added code comment

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get('/refreshFeed', function(req, res) {
 });
 
 app.get('/', function(req, res) {
+
+	// This can be replaced with a worker job, but this is the cheap way of doing it :)
 	if (feed.cache.length == 0 || feed.lastUpdated+1 > new Date().getDate()) {
 		console.log("Feed is empty or more than one day old!");
 		feed.updateFeed();
