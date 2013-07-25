c5bd7c6d51274255ac6118a0b2b63931431ee8f1
nikolai-neovada <nikolai@stasj.com>
2013-07-25 14:59:11 +0200

    Now includes Github

--- feed.js
+++ feed.js
@@ -1,7 +1,8 @@
 var feed = {}, async = require('async'),
 	Twitter = require('mtwitter'), 
 	Instagram = require('instagram-node-lib'),
-	config = require('./config');
+	config = require('./config'),
+	GitHubApi = require("github");
 
 var twitter = new Twitter({
   consumer_key: config.twitter.consumer_key,
@@ -14,8 +15,42 @@ Instagram.set('client_id', config.instagram.client_id);
 Instagram.set('client_secret', config.instagram.client_secret);
 Instagram.set('access_token', config.instagram.access_token);
 
+var github = new GitHubApi({
+    version: "3.0.0",
+    timeout: 5000
+});
+
 feed.cache = Array();
 feed.lastUpdated = new Date();
+
+
+feed.GithubMessage = function(item) {
+	switch (item.type) {
+		case 'PushEvent':
+			return item.payload.commits[0].message + " in " + item.repo.name;
+			break;
+		case 'FollowEvent':
+			return "Started following "+item.payload.target.login;
+			break;
+		default:
+			return "No info"
+			break;
+	}
+}
+
+feed.GithubLink = function(item) {
+	switch (item.type) {
+		case 'PushEvent':
+			return item.payload.commits[0].url;
+			break;
+		case 'FollowEvent':
+			return item.payload.target.url;
+			break;
+		default:
+			return "No info"
+			break;
+	}
+}
 
 feed.updateFeed = function() {
 	console.log("Starting feed update.");
@@ -29,7 +64,9 @@ feed.updateFeed = function() {
 						score: (item.retweeted ? item.retweeted : 0) + (item.favorited ? item.favorited : 0),
 						image: item.entities.media != null ? item.entities.media[0].media_url : null,
 						text: item.text,
-						date: new Date(item.created_at.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/, "$1 $2 $4 $3 UTC"))
+						date: new Date(item.created_at.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/, "$1 $2 $4 $3 UTC")),
+						type: 'twitter',
+						link: item.url
 					});
 				});
 				callback(err,result);
@@ -46,18 +83,40 @@ feed.updateFeed = function() {
 							score: item.likes.count,
 							image: item.images.standard_resolution.url,
 							text: item.caption ? item.caption.text : "",
-							date: new Date(parseInt(item.created_time) * 1000)
+							date: new Date(parseInt(item.created_time) * 1000),
+							type: 'instagram',
+							link: item.url
 						});
 					});
 					callback(null, result);
 				}
+			});
+		},
+		function(callback) {
+			github.events.getFromUser({
+			    user: "nikolaia",
+			    count: 15
+			}, function(err, res) {
+			    var result = new Array();
+				res.forEach(function(item) { 
+					if (item.type == 'PushEvent') // TODO: Add all Github eventTypes and make support for them.
+					result.push({
+						score: 0,
+						image: item.actor.avatar_url,
+						text: feed.GithubMessage(item),
+						date: new Date(item.created_at),
+						type: 'github',
+						link: feed.GithubLink(item)
+					});
+				});
+				callback(null, result);
 			});
 		}
 	], 
 	function(err, results) {
 		if (err) console.log(err);
 
-		feed.cache = results[0].concat(results[1]);
+		feed.cache = results[0].concat(results[1]).concat(results[2]);
 
 		function compare(a,b) {
 		  if (a.date > b.date)
@@ -66,6 +125,7 @@ feed.updateFeed = function() {
 		    return 1;
 		  return 0;
 		}
+
 		feed.cache.sort(compare);
 
 		lastUpdated = new Date();
